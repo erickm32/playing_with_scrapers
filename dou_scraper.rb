198c7501686ad1f6e75892226847c8c88857b146
Érick Moreira <erick@sementesrastreadas.com.br>
2024-11-06 08:41:00 -0300

    Tried to fix the script to click on the filter instead of just putting them on the url, because it wasnt actually showing the results now. But found a json object on the page, using it now

--- dou_scraper.rb
+++ dou_scraper.rb
@@ -1,22 +1,42 @@
 require 'selenium-webdriver'
-require 'httparty'
-require 'pdf-reader'
+require 'nokogiri'
 
 options = Selenium::WebDriver::Chrome::Options.new
 options.add_argument('--headless')
 driver = Selenium::WebDriver.for(:chrome, options:)
 
+pp 'opened, navigating...'
 url = "https://www.in.gov.br/leiturajornal?secao=dou3&data=#{Date.today.strftime('%d-%m-%Y')}"
-other_filters = '&org=Minist%C3%A9rio%20da%20Gest%C3%A3o%20e%20da%20Inova%C3%A7%C3%A3o%20em%20Servi%C3%A7os%20P%C3%BAblicos&org_sub=Secretaria%20de%20Servi%C3%A7os%20Compartilhados&ato=Edital'
-driver.navigate.to url + other_filters
+# other_filters = '&org=Minist%C3%A9rio%20da%20Gest%C3%A3o%20e%20da%20Inova%C3%A7%C3%A3o%20em%20Servi%C3%A7os%20P%C3%BAblicos&org_sub=Secretaria%20de%20Servi%C3%A7os%20Compartilhados&ato=Edital'
+# driver.navigate.to url + other_filters
+driver.navigate.to url
 
 pp 'sleeping...'
-sleep(5)
+sleep(3)
 
-pp 'looking for css...'
-# driver.find_elements(:css, '#hierarchy_content > ul > li > div') or
-search_results = driver.find_elements(:css, '.resultado')
+# pp 'clicking stuff'
+# # driver.find_element(:css, '#slcOrgs').click
+# # sleep(0.5)
 
-pp search_results.map(&:text)
+# driver.find_element(:css, '#slcOrgs > option[value~=Gestão] ').click
+# sleep(0.5)
+
+# pp 'looking for css...'
+# # driver.find_elements(:css, '#hierarchy_content > ul > li > div') or
+# search_results = driver.find_elements(:css, '.resultado')
+
+# pp search_results.map(&:text)
+
+html = Nokogiri::HTML(driver.page_source)
+script_tag = html.css('script#params')
+data = JSON.parse(script_tag.text)['jsonArray']
+related_data = data
+               .filter { |d| d['hierarchyStr'].include?('Ministério da Gestão e da Inovação') }
+               .map { |d| d['content'] }
+pp related_data
+
+5.times { pp '' }
+
+related_data.filter { |d| d.include?('Convocação') }.each { |d| pp d }
 
 driver.close
