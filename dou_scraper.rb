9f2a5a99f90b26066018b39dcbe7e0151c3af7ea
Érick Moreira <erick@sementesrastreadas.com.br>
2024-11-06 08:56:38 -0300

    No need for selenium usage as the site returns data with a simple http get. Removing some comments and using httparty

--- dou_scraper.rb
+++ dou_scraper.rb
@@ -1,42 +1,28 @@
-require 'selenium-webdriver'
+require 'json'
+require 'httparty'
 require 'nokogiri'
 
-options = Selenium::WebDriver::Chrome::Options.new
-options.add_argument('--headless')
-driver = Selenium::WebDriver.for(:chrome, options:)
+puts 'navigating...'
+url = "https://www.in.gov.br/leiturajornal?secao=dou3&data=#{Time.now.strftime('%d-%m-%Y')}"
+response = HTTParty.get(url)
 
-pp 'opened, navigating...'
-url = "https://www.in.gov.br/leiturajornal?secao=dou3&data=#{Date.today.strftime('%d-%m-%Y')}"
-# other_filters = '&org=Minist%C3%A9rio%20da%20Gest%C3%A3o%20e%20da%20Inova%C3%A7%C3%A3o%20em%20Servi%C3%A7os%20P%C3%BAblicos&org_sub=Secretaria%20de%20Servi%C3%A7os%20Compartilhados&ato=Edital'
-# driver.navigate.to url + other_filters
-driver.navigate.to url
+if response.success?
+  html = Nokogiri::HTML(response.body)
+  script_tag = html.css('script#params')
+  data = JSON.parse(script_tag.text)['jsonArray']
+  related_data = data
+                 .filter { |d| d['hierarchyStr'].include?('Ministério da Gestão e da Inovação') }
+                 .map { |d| d['content'] }
+  pp related_data
 
-pp 'sleeping...'
-sleep(3)
+  5.times { pp '' }
 
-# pp 'clicking stuff'
-# # driver.find_element(:css, '#slcOrgs').click
-# # sleep(0.5)
+  puts 'looking for Convocação'
+  filtered = related_data.filter { |d| d.include?('Convocação') }
+  puts "found #{filtered.length} matches"
+  filtered.each { |d| pp d }
 
-# driver.find_element(:css, '#slcOrgs > option[value~=Gestão] ').click
-# sleep(0.5)
-
-# pp 'looking for css...'
-# # driver.find_elements(:css, '#hierarchy_content > ul > li > div') or
-# search_results = driver.find_elements(:css, '.resultado')
-
-# pp search_results.map(&:text)
-
-html = Nokogiri::HTML(driver.page_source)
-script_tag = html.css('script#params')
-data = JSON.parse(script_tag.text)['jsonArray']
-related_data = data
-               .filter { |d| d['hierarchyStr'].include?('Ministério da Gestão e da Inovação') }
-               .map { |d| d['content'] }
-pp related_data
-
-5.times { pp '' }
-
-related_data.filter { |d| d.include?('Convocação') }.each { |d| pp d }
-
-driver.close
+else
+  pp 'failed to reach'
+  pp response.inspect
+end
